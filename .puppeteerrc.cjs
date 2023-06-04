5d436f7e2885ee7022645aaf11865436e15d9686
Dennis Hodges <fermentationist@gmail.com>
2023-06-04 16:51:00 -0500

    Added .puppeteerrc.cjs, in attempt to fix puppeteer issues on Render.com. Will not work locally with this change.

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const { join } = require("path");
+
+const PROD_MODE = process.env.NODE_ENV === "production";
+const cachePath = join(__dirname, ".cache", "puppeteer");
+
+const config = PROD_MODE ? {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: cachePath,
+} : {};
+
+module.exports = config;
