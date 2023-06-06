2208fa828152c6b2f39805ff4c0a8e9822e45e33
Dennis Hodges <fermentationist@gmail.com>
2023-06-06 09:28:19 -0500

    added some logging to getFAQ

--- server/services/getFAQ.js
+++ server/services/getFAQ.js
@@ -5,8 +5,8 @@ const URL_TO_SCRAPE = process.env.URL_TO_SCRAPE;
 export const FAQ_UPDATE_INTERVAL = 1000 * 60 * 60 * 24; // 24 hours
 const faq = {
   data: null,
-  expires: null
-}
+  expires: null,
+};
 
 export default async function getFaq() {
   if (faq.data && faq.expires > Date.now()) {
@@ -15,14 +15,16 @@ export default async function getFaq() {
   }
   try {
     // scrape data
+    console.log(`scraping ${URL_TO_SCRAPE} for FAQ data`);
     const data = await PageScraper.scrape(URL_TO_SCRAPE);
     faq.data = data;
     faq.expires = Date.now() + FAQ_UPDATE_INTERVAL;
+    console.log("FAQ data updated");
     return data;
   } catch (err) {
     console.error(err);
     // scraping failed, return stale data or empty string
-    console.log("scraping failed, returning stale faq data")
+    console.log("scraping failed, returning stale or empty FAQ data");
     return faq.data ?? "";
   }
 }
