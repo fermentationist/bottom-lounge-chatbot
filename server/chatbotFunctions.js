ffdc7ff8770d2665331c500bfdd45b96c456677e
Dennis Hodges <fermentationist@gmail.com>
2023-06-21 00:41:17 -0500

    UPdated getUpcomingEvents to account for paginated results

--- server/chatbotFunctions.js
+++ server/chatbotFunctions.js
@@ -9,14 +9,14 @@ export const chatbotFunctionDefinitions = [
   {
     name: "getUpcomingEvents",
     description:
-      "Get all upcoming events at Bottom Lounge (if called with no arguments), or get upcoming events on a specific date or date range. For each event, returns the name, date, time, price, status, ageRestrictions (either the minimum age or \"All ages\"), url and any additional notes.",
+      'Get all upcoming events at Bottom Lounge (if called with no arguments), or get upcoming events on a specific date or date range. For each event, returns the name, date, time, price, status, ageRestrictions (either the minimum age or "All ages"), url and any additional notes.',
     parameters: {
       type: "object",
       properties: {
         dateTimeOrDateTimeRange: {
           type: "string",
           description:
-            "A start datetime or datetime range in ISO format, like 2023-09-01T00:00:00Z or 2023-09-01T00:00:00Z..2023-09-02T00:00:00Z. Be sure to adjust for Chicago time by including the current offset from UTC. For example, if you want to see events on September 1, 2023, you would use 2023-09-01T00:00:00-05:00..2023-09-01T23:59:59-05:00. If passed only a start datetime, will search for all events from that datetime onward. If passed no arguments, will search for all events from the current datetime onward.",
+            "A start datetime or datetime range in ISO format, like 2023-09-01T00:00:00Z or 2023-09-01T00:00:00Z..2023-09-02T00:00:00Z. Be sure to adjust for Chicago time by including the current offset from UTC. For example, if you want to see events on September 1, 2023, you would use 2023-09-01T00:00:00-05:00..2023-09-01T23:59:59-05:00. If passed only a start datetime, will search for all events from that datetime onward. If undefined, will search for all events from the current datetime onward.",
         },
         keyword: {
           type: "string",
@@ -36,21 +36,36 @@ export const chatbotFunctionDefinitions = [
 ];
 
 async function getUpcomingEvents(args) {
-  const { dateTimeOrDateTimeRange, keyword } = (args && JSON.parse(args)) ?? {};
-  const [startDate, endDate] = (dateTimeOrDateTimeRange ?? "").split("..");
-  const events = await getUpcomingBLEvents(startDate || void 0, endDate, keyword);
-  const eventList = events.length && events
-    .map(
-      ({ name, url, date, time, price, status, notes, ageRestrictions }) =>
-        `${date} at ${time}: ${name} - ${price} - status: ${status} - ${url} - age restrictions: ${ageRestrictions}${
-          notes ? ` - please note: ${notes}` : ""
-        }`
-    )
-    .join("\n");
-  return eventList ? (
-    eventList +
-    ` \n(All times are in Chicago time. Current time in Chicago: ${getCurrentDateAndTime()})`
-  ) : "No events found matching those criteria.";
+  try {
+    const { dateTimeOrDateTimeRange, keyword, ageRestrictions } =
+      (args && JSON.parse(args)) ?? {};
+    const [startDate, endDate] = (dateTimeOrDateTimeRange ?? "").split("..");
+    const events = await getUpcomingBLEvents(
+      startDate || void 0,
+      endDate,
+      keyword
+    );
+    const filteredEvents = ageRestrictions
+      ? events.filter((event) => event.ageRestrictions === ageRestrictions)
+      : events;
+    const eventList =
+      filteredEvents.length &&
+      filteredEvents
+        .map(
+          ({ name, url, date, time, price, status, notes, ageRestrictions }) =>
+            `${date} at ${time}: ${name} - ${price} - status: ${status} - ${url} - age restrictions: ${ageRestrictions}${
+              notes ? ` - please note: ${notes}` : ""
+            }`
+        )
+        .join("\n");
+    return eventList
+      ? eventList +
+          ` \n(All times are in Chicago time. Current time in Chicago: ${getCurrentDateAndTime()})`
+      : "No events found matching those criteria.";
+  } catch (err) {
+    console.error(err);
+    return "Something went wrong while contacting the Ticketmaster API. Please try again later.";
+  }
 }
 
 function getCurrentDateAndTime() {
--- server/services/ticketmaster.js
+++ server/services/ticketmaster.js
@@ -9,53 +9,96 @@ const BOTTOM_LOUNGE_VENUE_ID = "KovZpaoyCe";
 
 const getUpcomingEvents = async (startDate, endDate, keyword) => {
   startDate = startDate === "now" ? new Date().toISOString() : startDate;
-  const url = `${TICKETMASTER_DISCOVERY_API_EVENTS_URL}?apikey=${TICKETMASTER_API_KEY}&venueId=${BOTTOM_LOUNGE_VENUE_ID}&sort=date,asc${startDate ? `&startDateTime=${startDate}` : ""}${endDate ? `&endDateTime=${endDate}` : ""}${keyword ? `&keyword=${encodeURIComponent(keyword)}` : ""}`;
-  console.log("TM url:", url)
-  const response = await fetch(url,{
-    method: "GET",
-  }).then(res => {
-    if (res.ok) {
-      return res.json();
-    } 
-    return Promise.reject(res);
-  }).catch((err) => {
-    console.error(err);
-    return Promise.reject(err);
-  });
-  const events = response?._embedded?.events ?? [];
-  // filter out test events
-  const realEvents = events.filter(event => !event.test);
-  const eventData = realEvents.map(event => {
-    const { name, url, dates, priceRanges, ageRestrictions, pleaseNote: note } = event;
-    const { start } = dates;
-    const { localDate, localTime } = start;
-    const { min, max } = (priceRanges && priceRanges[0]) ?? {};
-    const price = min ? (min === max) || !max ? `$${min}` : `$${min} - $${max}` : `? (see ${url} for details)`;
-    const status = dates?.status?.code;
-    const result = {
-      name,
-      url,
-      date: localDate,
-      time: localTime,
-      price,
-      status,
-      ageRestrictions: "All ages",
-    };
-    if (ageRestrictions) {
-      const { legalAgeEnforced, age } = ageRestrictions;
-      result.ageRestrictions = legalAgeEnforced ? `${age}+` : `All ages`;
-    }
-    if (note) {
-      result.note = note;
-      const ageRestrictionMatch = note.match(/(17|18|21)/);
-      if (ageRestrictionMatch) {
-        result.ageRestrictions = `${ageRestrictionMatch[0]}+`;
+  const pages = [];
+  const baseURL = `${TICKETMASTER_DISCOVERY_API_EVENTS_URL}?apikey=${TICKETMASTER_API_KEY}&venueId=${BOTTOM_LOUNGE_VENUE_ID}&sort=date,asc${
+    startDate ? `&startDateTime=${startDate}` : ""
+  }${endDate ? `&endDateTime=${endDate}` : ""}${
+    keyword ? `&keyword=${encodeURIComponent(keyword)}` : ""
+  }&size=200`;
+  const getPage = async (pageNum) => {
+    const url = `${baseURL}&page=${pageNum}`;
+    console.log("TM url:", url);
+    const response = await fetch(url, {
+      method: "GET",
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(res);
+      })
+      .catch((err) => {
+        console.error(err);
+        return Promise.reject(err);
+      });
+    const { totalPages } = response?.page ?? {};
+    const events = response?._embedded?.events ?? [];
+    // filter out test events
+    const realEvents = events.filter((event) => !event.test);
+    const eventData = realEvents.map((event) => {
+      const {
+        name,
+        url,
+        dates,
+        priceRanges,
+        ageRestrictions,
+        pleaseNote: notes,
+      } = event;
+      const { start } = dates;
+      const { localDate, localTime } = start;
+      const { min, max } = (priceRanges && priceRanges[0]) ?? {};
+      const price = min
+        ? min === max || !max
+          ? `$${min}`
+          : `$${min} - $${max}`
+        : `? (see ${url} for details)`;
+      const status = dates?.status?.code;
+      const result = {
+        name,
+        url,
+        date: localDate,
+        time: localTime,
+        price,
+        status,
+        ageRestrictions: "All ages",
+      };
+      if (ageRestrictions) {
+        const { legalAgeEnforced, age } = ageRestrictions;
+        result.ageRestrictions = legalAgeEnforced ? `${age}+` : `All ages`;
       }
+      if (notes) {
+        result.notes = notes;
+        const ageRestrictionMatch = notes.match(/(17|18|21)/);
+        if (ageRestrictionMatch) {
+          result.ageRestrictions = `${ageRestrictionMatch[0]}+`;
+        }
+      }
+      return result;
+    });
+    console.log("eventData:", eventData);
+    return {
+      data: eventData,
+      totalPages
     }
-    return result;
-  });
-  console.log("eventData:", eventData);
-  return eventData;
+  };
+
+  let pageNum = 0;
+  let totalPages = 1;
+  while (pageNum <= totalPages) {
+    console.log("pageNum:", pageNum);
+    const page = await getPage(pageNum);
+    if (page) {
+      if (page.totalPages) {
+        totalPages = page.totalPages;
+      }
+      pages.push(page.data);
+    } else {
+      break;
+    }
+    console.log("totalPages:", totalPages);
+    pageNum++;
+  }
+  return pages.flat();
 };
 
 export default getUpcomingEvents;
